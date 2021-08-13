eeea929fad6b8ce795b0664eeca05d19beacc7bc
cs13engineer <75882819+cs13engineer@users.noreply.github.com>
2021-08-13 12:34:59 +0530

    Create greetingMe.go

---
+++ greetingMe.go
@@ -0,0 +1,14 @@
+// Package greeting greets the user in English.
+package greeting
+
+import "fmt"
+
+// Hello prints the string "Hello!".
+func Hello() {
+	fmt.Println("Hello Amol!")
+}
+
+// Hi prints the string "Hi!".
+func Hi() {
+	fmt.Println("Hi How are you!")
+}
